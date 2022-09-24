37ad84a2f483eb0680d07fc7fea35fdba2f96814
MichaelBoyo <boyomichaelbidemi@gmail.com>
2022-09-24 15:02:57 +0100

    product controller

--- src/main/java/com/tbthecoder/smallamazon/controller/ProductController.java
+++ src/main/java/com/tbthecoder/smallamazon/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.tbthecoder.smallamazon.controller;
 
 import com.tbthecoder.smallamazon.dtos.ProductRequest;
 import com.tbthecoder.smallamazon.exceptions.ProductNotFoundException;
+import com.tbthecoder.smallamazon.models.Product;
 import com.tbthecoder.smallamazon.services.interfaces.ProductService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,16 +21,22 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<?> addProduct(@RequestBody ProductRequest request) {
         log.info("adding product {}", request);
-        return new ResponseEntity<>(productService.saveProduct(request), HttpStatus.OK);
+        return ResponseEntity.ok(productService.saveProduct(request));
     }
     @PreAuthorize("hasRole('ROLE_SUPER_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable String id) throws ProductNotFoundException {
-        return new ResponseEntity<>(productService.deleteProduct(id), HttpStatus.OK);
+        return ResponseEntity.ok(productService.deleteProduct(id));
     }
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_SELLER')")
     @GetMapping("/all")
     public ResponseEntity<?> getAllProducts() {
-        return new ResponseEntity<>(productService.findAllProducts(), HttpStatus.OK);
+        return ResponseEntity.ok(productService.findAllProducts());
     }
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_SELLER')")
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable String id) throws ProductNotFoundException {
+        return ResponseEntity.ok(productService.getProduct(id));
+    }
+
 }
